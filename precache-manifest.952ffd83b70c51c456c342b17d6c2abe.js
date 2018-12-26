46c31222ce74dfb5f1232c2da7ae4e35e3fa0caa
Travis CI <sh@signalwerk.ch>
2018-12-26 05:08:26 +0000

    Deploy to GitHub Pages: 8997470537647b9688b07ced8c0c003e1cff75ee

---
+++ precache-manifest.952ffd83b70c51c456c342b17d6c2abe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acca97296962923b93b",
+    "url": "/static/css/main.8033b85c.chunk.css"
+  },
+  {
+    "revision": "6acca97296962923b93b",
+    "url": "/static/js/main.6acca972.chunk.js"
+  },
+  {
+    "revision": "c5b6cfba232421f84941",
+    "url": "/static/css/1.61ec90d9.chunk.css"
+  },
+  {
+    "revision": "c5b6cfba232421f84941",
+    "url": "/static/js/1.c5b6cfba.chunk.js"
+  },
+  {
+    "revision": "abbab2c44d2de6685d18",
+    "url": "/static/js/2.abbab2c4.chunk.js"
+  },
+  {
+    "revision": "d6b4739ec5a8209d57c1",
+    "url": "/static/js/3.d6b4739e.chunk.js"
+  },
+  {
+    "revision": "86a955df7d49f140c118",
+    "url": "/static/js/4.86a955df.chunk.js"
+  },
+  {
+    "revision": "a5aa7b74d6ea95c3698d",
+    "url": "/static/js/runtime~main.a5aa7b74.js"
+  },
+  {
+    "revision": "3dfbeaf1bf0cefe0d3b947643c2d47cc",
+    "url": "/index.html"
+  }
+];
